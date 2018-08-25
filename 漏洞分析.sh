9fb9ccd18b831f73b19d75abef8fa66a7eb2fddb
shihr <480939289@qq.com>
2018-08-25 23:31:38 +0800

    添加 漏洞分析.sh

---
+++ 漏洞分析.sh
@@ -0,0 +1,31 @@
+kali-tools
+
+漏洞分析
+
+bbqsql:0 强大的sql盲注工具。不会用
+bed:0 网络协议检查工具。检查潜在的缓冲区溢出，格式化字符串等
+CAT：0 扫描cisco路由器的常见漏洞
+cge.pl：0 Cisco Global Exploiter（CGE），简单快速的安全检测工具
+cisco-ocs：0 大规模Cisco扫描工具
+cisco-torch：0 大规模Cisco扫描工具
+copy-router-config:0 从运行SNMP（简单网络管理协议）的Cisco设备复制配置文件
+	copy-router-config.pl：0 
+	merge-router-config.pl：0 通过SNMP合并Cisco路由配置
+dbpwaudit:0 为几种数据库引擎执行在线密码质量审查。Microsoft SQL Server 2000/2005、Oracle 8/9/10/11、IBM DB2 Universal Database、MySQL
+doona：0 是bed（BED，信息收集.sh有介绍）工具的一个分支。大量增加和改变了bed的功能
+dotdotpwn:0 见信息收集.sh
+hexorbase:0 图形化界面。从一个集中位置同时管理和审查多个数据库服务，能够执行SQL查询和暴力攻击普通的数据库服务器(MySQL, SQLite, Microsoft SQL Server, Oracle, PostgreSQL )
+inguma:0 渗透测试工具包。发现主机，信息收集，模糊攻击，暴力破解用户名密码，exploits（一种对计算机系统的攻击，尤其是利用特定的漏洞）。只能去网页下载了，2014年停止更新
+jsql:0 轻量级、从远程服务找到数据库信息。图形界面
+lynis：0 审查工具。审查和强化Unix、Linux基础系统。检测自身的系统
+nmap：0 见信息收集.sh
+ohrwurm:0 一个小巧的RTP（Real-time Transport Protocol实时传送协议，一种网络传输协议）模糊攻击工具，已经成功地测试了少数的SIP（Session Initiation Protocol,会话初始协议）电话
+openvas:0 一个几种服务和工具的框架，提供全面而强力的漏洞扫描和漏洞管理解决方案
+	openvas-setup：0 第一步，设置openvas
+	openvas-feed-update：0 更新签名
+	openvasmd：0 开放的漏洞评估系统管理器
+	openvas-manage-certs：0 对openvas的安装 管理证书
+	openvas-migrate-to-postgres：0 将管理器数据库从SQLite3迁移到Postgres
+	openvas-portnames-update：0 从端口名称xml文件更新端口名字数据
+	openvassd：0 开放的漏洞评估系统扫描器
+oscaner:0 Oracle评估框架。基于插件体系结构
