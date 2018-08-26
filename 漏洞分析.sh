5fd32082d16b45dab8b197cc29b746c0fe1cc692
shihr <480939289@qq.com>
2018-08-26 16:54:07 +0800

    完成漏洞分析

--- 漏洞分析.sh
+++ 漏洞分析.sh
@@ -1,6 +1,7 @@
 kali-tools
 
 漏洞分析
+常见名词 fuzzer： 自动的输入非法字符、任意数据等的测试工具
 
 bbqsql:0 强大的sql盲注工具。不会用
 bed:0 网络协议检查工具。检查潜在的缓冲区溢出，格式化字符串等
@@ -15,11 +16,11 @@ dbpwaudit:0 为几种数据库引擎执行在线密码质量审查。Microsoft S
 doona：0 是bed（BED，信息收集.sh有介绍）工具的一个分支。大量增加和改变了bed的功能
 dotdotpwn:0 见信息收集.sh
 hexorbase:0 图形化界面。从一个集中位置同时管理和审查多个数据库服务，能够执行SQL查询和暴力攻击普通的数据库服务器(MySQL, SQLite, Microsoft SQL Server, Oracle, PostgreSQL )
-inguma:0 渗透测试工具包。发现主机，信息收集，模糊攻击，暴力破解用户名密码，exploits（一种对计算机系统的攻击，尤其是利用特定的漏洞）。只能去网页下载了，2014年停止更新
+inguma:0 渗透测试工具包。发现主机，信息收集，fuzz攻击，暴力破解用户名密码，exploits（一种对计算机系统的攻击，尤其是利用特定的漏洞）。只能去网页下载了，2014年停止更新
 jsql:0 轻量级、从远程服务找到数据库信息。图形界面
 lynis：0 审查工具。审查和强化Unix、Linux基础系统。检测自身的系统
 nmap：0 见信息收集.sh
-ohrwurm:0 一个小巧的RTP（Real-time Transport Protocol实时传送协议，一种网络传输协议）模糊攻击工具，已经成功地测试了少数的SIP（Session Initiation Protocol,会话初始协议）电话
+ohrwurm:0 一个小巧的RTP（Real-time Transport Protocol实时传送协议，一种网络传输协议）fuzz攻击工具，已经成功地测试了少数的SIP（Session Initiation Protocol,会话初始协议）电话
 openvas:0 一个几种服务和工具的框架，提供全面而强力的漏洞扫描和漏洞管理解决方案
 	openvas-setup：0 第一步，设置openvas
 	openvas-feed-update：0 更新签名
@@ -29,3 +30,14 @@ openvas:0 一个几种服务和工具的框架，提供全面而强力的漏洞�
 	openvas-portnames-update：0 从端口名称xml文件更新端口名字数据
 	openvassd：0 开放的漏洞评估系统扫描器
 oscaner:0 Oracle评估框架。基于插件体系结构
+powerfuzzer：0 图形界面。高度自动化和完全定制的web fuzzer测试工具（基于HTTP协议）。web应用程序漏洞扫描器。跨站点脚本、注入、CRLF（CRLF字符注入攻击，通常被解码为\r\n）、HTTP 500 状态。看不懂怎么用的
+sfuzz：0 simple fuzz，快速可配置的黑盒测试工具
+sidguesser:0 根据预定义的字典文件猜测Oracle数据库的 sids（安全标识符，标识账户的唯一号码）/instances，速度慢但是有效
+siparmyknife：0 一个fuzzer，跨站脚本攻击、SQL注入、log注入、格式化字符串、缓冲溢出等
+sqlmap:0 很强大的渗透测试工具，自动化检测和开发SQL注入缺陷和接管数据库服务器
+sqlninja：0 在一个使用Micorsoft SQL Server作为后台的web应用程序上开发SQL注入漏洞
+sqlsus：0 高效的MySQL注入和接管工具，使用之前需要生成和修改配置文件
+thc-ipv6：0 见信息收集.sh
+tnscmd10g:0 在tcp端口1521上探测Oracle的tnslsnr（与Oracle TNS Listener关联）线程（可以让其他用户连接Oracle数据库）
+Unix-privesc-checker：0 在Unix系统上，试图找到错误的配置以便于允许本地非特权用户给其他用户升级特权或访问本地应用程序（e.g.database）
+yersinia：0 有图形界面。执行两层攻击的框架。利用不同的网络协议的弱点。
